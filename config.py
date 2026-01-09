ed082a4381faf0f462762d5fed8f39cbb519567f
Vivitha269 <a.vivithavivitha26@gmail.com>
2026-01-09 22:29:05 +0530

    Updated config.py to parse Firebase credentials as JSON from env var

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, messaging
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
-cred = credentials.Certificate(os.getenv('FIREBASE_CREDENTIALS'))
+# Load Firebase credentials from env var (JSON string)
+firebase_creds = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
